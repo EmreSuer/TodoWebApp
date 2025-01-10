96b533b60af71efe421a1d172b5b7c12bde74f61
EmreSuer <emresuer11@hotmail.com>
2025-01-11 01:14:00 +0300

    Increase password length

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     email_verified = db.Column(db.Boolean, default=False)
     verification_token = db.Column(db.String(255), unique=True)
     token_expiration = db.Column(db.DateTime)
